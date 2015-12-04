0c291dd48b84f2bf0c095a7d33e80858c65a266e
Arthur RL <arlmessenger@free.fr>
2015-12-04 22:34:57 +0100

    YOLOOOOOOOOO

---
+++ Assets/Scripts/SmashCount.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SmashCount : MonoBehaviour 
+{
+	public int smashcount;
+	public int NumberPerSmashes;
+	public Text CountText;
+
+	void start()
+	{	
+		smashcount = 0;
+		CountText.text = "smash number: " + smashcount.ToString ();
+	}
+
+	void SmashNumber()
+	{
+		smashcount = smashcount + NumberPerSmashes;
+		CountText.text = "smash number: " + smashcount.ToString ();
+	}
+
+}
